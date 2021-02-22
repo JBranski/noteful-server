cb791f722af9f641405a0420c6c9455667548aee
JBranski <jonathan.branski96@gmail.com>
2021-02-21 21:21:31 -0600

    msg

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require( 'express' );
 const app = express();
+const cors = require('cors')
 const folderRouter = require( './routers/folderRouter' );
 const noteRouter = require( './routers/noteRouter' );
 
+const corsOptions = {
+	origin: 'https://notefulproject.vercel.app',
+	optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use( '/api/folders', folderRouter );
 app.use( '/api/notes', noteRouter );
 
