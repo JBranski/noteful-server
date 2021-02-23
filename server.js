cc73a03723e010f277cb1b65e532e7f129d576da
JBranski <jonathan.branski96@gmail.com>
2021-02-23 08:35:08 -0600

    msg

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const knex = require( 'knex' );
 const app = require( './app' );
+const {PORT, DATABASE_URL} = require( './config' );
 
 const db = knex({
     client : 'pg',
-    connection : 'postgres://gvqrgflxbfzitj:4369b01e21f49629bc9ad3632fdeb7b4a81d2d9f036bf1539aaa5b8aa792f2c8@ec2-52-205-3-3.compute-1.amazonaws.com:5432/dflr39evjdi8fv'
+    connection : DATABASE_URL
 });
 
 app.set( 'db', db );
 
-app.listen( 8080, () => {
+app.listen( PORT, () => {
     console.log( "Server running in port 8080." );
 });
